9e815dc06c1cf581f3e2d92f71b07bc92ed6bf89
Hariharasudan <harisanth123@gmail.com>
2019-02-07 23:46:53 +0530

    Ingredient create and delete

--- js/ingredient-tab.js
+++ js/ingredient-tab.js
@@ -7,7 +7,8 @@ IngredientTab.prototype.init = function() {
 	_this.registerEvents();
 };
 IngredientTab.prototype.render = function render() {
-	var ingredientJson = [{
+	var _this = this;
+	_this.ingredientJsonArr = [{
 							"id": 1,
 							"name": "sugar",
 							"tamilName": "சர்க்கரை",
@@ -23,16 +24,34 @@ IngredientTab.prototype.render = function render() {
 						}]
 };
 IngredientTab.prototype.registerEvents = function render() {
+	var _this = this;
 	$(document).ready(function(){
 		$("#id_createIngredientForm").submit(function() {
-			//Generate Ingredient Object dynamically and push to ingredient file
+			var length = _this.ingredientJsonArr&& _this.ingredientJsonArr.length ? _this.ingredientJsonArr.length : 0;
+			var id = _this.ingredientJsonArr[length-1].id+1;
+
+			var name = $("#id_name").val().trim();
+			var tamilName = $("#id_tamilName").val().trim();
+			var unit = $("#id_unit").val();
+
 			var ingredientObj = {
-					"id": 3,
-					"name": "sugar",
-					"tamilName": "சர்க்கரை",
-					"category": "provisions",
-					"unitOfMeasure": "kilogram"
+					"id": id,
+					"name": name,
+					"tamilName": tamilName,
+					"category": "provisions", // Need to finalize the category
+					"unitOfMeasure": unit
 					};
+			//File write operation
+			//_this.ingredientJsonArr.push(ingredientObj)
+			window.location.reload();
 		});
+		
+		$(".cls_delete").click(function() {
+	       var idx = $(this).attr("idx");
+	       var isDelete = confirm("You wish to delete this ingredient?");
+	       if (isDelete == true) {
+	         $(".ingredient_"+idx).remove();
+	       }
+	    });
 	});
 }
