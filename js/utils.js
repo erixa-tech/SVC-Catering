af00c7da1ff71f93d27625c9bebbda1c82e3fac7
kaviyarasane <kaviyarasan.e@gmail.com>
2019-02-17 18:27:43 +0530

    header separate file

--- js/utils.js
+++ js/utils.js
@@ -54,3 +54,44 @@ function getFileValue(){
 	});
 
 }
+
+/*function loadFile(file, elem){
+
+	$.ajax({
+      crossOrigin: false,
+      url: file,
+      success: function(data) {
+        alert("asd");	
+      }
+    });
+
+	var headers = new Headers();
+	headers.append("Access-Control-Allow-Origin", "*");
+	headers.append("Access-Control-Allow-Credentials", true);
+	headers.append("Access-Control-Allow-Methods", "GET");
+	$.ajax({
+      type: "GET",
+	  xhrFields: {
+        withCredentials: true
+      },
+      dataType: 'json',
+	  headers: {"Access-Control-Allow-Origin": "*"},
+      url: file,
+      success: function(data) {
+        elem.html(data);
+      },
+      error: function(data) {
+      	console.log(data);
+      }
+    });
+
+	var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+}*/
