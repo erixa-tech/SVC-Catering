a364de31d713d7bd69f19f695d621f64b5be206b
Hariharasudan <harisanth123@gmail.com>
2019-02-21 22:04:30 +0530

    Create Recipe, Ingredient Map DOM clone

--- js/recipe-tab.js
+++ js/recipe-tab.js
@@ -6,6 +6,7 @@ function RecipeTab(){
 RecipeTab.prototype.init = function() {
 	var _this = this;
 	_this.render();
+	_this.registerEvents();
 };
 RecipeTab.prototype.render = function() {
 	var _this = this;
@@ -140,4 +141,43 @@ RecipeTab.prototype.render = function() {
 		}
 	}
 	$("#id_recipeContent_tab").append(renderHtml);
-}
+};
+
+RecipeTab.prototype.registerEvents = function() {
+	var _this = this;
+	$(document).ready(function(){
+		$(document).on("click", ".cls_addIngredient_sf", function(){
+			var elemToAdd = $(_this.getIngredientMapRow());
+			cloneDOM(elemToAdd, $('.createRecipeIngredientMap'));
+			/*addOptionsToSelectViaElem(_this.dummyRecipies, $('.cls_receipeCategory_sf')[$('.cls_receipeCategory_sf').length-1]);*/
+		});
+	});
+};
+
+RecipeTab.prototype.getIngredientMapRow = function() {
+	var _this = this;
+	var renderHtmlMapRow = [];
+	
+	renderHtmlMapRow += '    <div class="row ingredientMapRow mt-2 mb-1">'
+		              + '        <div class="col">'
+		              + '                <select class="form-control" id="id_ingredientCategory_recipe" name="ingredientCategory"></select>'
+		              + '        </div>'
+		              + '        <div class="col">'
+		              + '            <select class="form-control" id="id_ingredientName_recipe" name="ingredientName"></select>'
+		              + '        </div>'
+		              + '        <div class="col">'
+		              + '            <select class="form-control" id="id_ingredientUnit_recipe" name="ingredientUnit" disabled></select>'
+		              + '        </div>'
+		              + '		 <div class="col">'
+		              + '			 <input type="text" class="form-control" required id="id_qunatity_recipe" placeholder="Enter Quantity" name="qunatity">'
+		              + '		 </div>'
+		              + '		 <div class="col">'
+		              + '		     <button class="btn btn-success cls_addIngredient_sf">Add</button>'
+		              + '		 </div>'
+		              + '        <div class="col">'
+		  			  +             ($('.ingredientMapRow').length>0 ? '<i class="fa fa-minus-circle cls_removeCurrentIngredientMap" style="font-size:25px;color:red"></i>' : '')
+		  			  + '        </div>'
+		              + '	</div>';
+			
+	return renderHtmlMapRow;
+};
